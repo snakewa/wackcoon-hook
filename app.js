a4d2584a684e064ef4243c465aa046ada8c78ea7
Christoph Heidelbach <christoph.heidelbach@yahoo.com>
2017-09-30 10:10:22 +0200

    New Setup and bugfix

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.get('/', function (req, res) {
 	console.log('get /');
 });
 
-app.get('/payload', function (req, res) {
+app.get('/stua', function (req, res) {
     res.sendStatus(200);
-	console.log('get /payload');
+	console.log('get /stua');
 });
 
-app.post('/payload', function (req, res) {
+app.post('/stua/beleg', function (req, res) {
 	//verify that the payload is a push from the correct repo
 	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
 	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
@@ -24,26 +24,26 @@ app.post('/payload', function (req, res) {
 	console.log('pulling code from GitHub...');
 
 	// reset any changes that have been made locally
-	exec('git -C ~/projects/wackcoon-device reset --hard', execCallback);
+	//exec('git -C ~/projects/wackcoon-device reset --hard', execCallback);
 
 	// and ditch any files that have been added locally too
-	exec('git -C ~/projects/wackcoon-device clean -df', execCallback);
+	//exec('git -C ~/projects/wackcoon-device clean -df', execCallback);
 
 	// now pull down the latest
-	exec('git -C ~/projects/wackcoon-device pull -f', execCallback);
+	//exec('git -C ~/projects/wackcoon-device pull -f', execCallback);
 
 	// and npm install with --production
-	exec('npm -C ~/projects/wackcoon-device install --production', execCallback);
+	//exec('npm -C ~/projects/wackcoon-device install --production', execCallback);
 
 	// and run tsc
-	exec('tsc', execCallback);
+	//exec('tsc', execCallback);
+
+  res.sendStatus(200);
+  res.end();
 });
 
-res.sendStatus(200);
-res.end();
-
-app.listen(5000, function () {
-	console.log('listening on port 5000')
+app.listen(5029, function () {
+	console.log('App listening on port 5029')
 });
 
 function execCallback(err, stdout, stderr) {
