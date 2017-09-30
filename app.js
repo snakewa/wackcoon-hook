d00691302baedb754fe1bf0a9c021d38cd4d0ed9
Christoph Heidelbach <christoph.heidelbach@yahoo.com>
2017-09-30 10:29:32 +0200

    Add git pull for own repo

--- app.js
+++ app.js
@@ -20,24 +20,12 @@ app.post('/stua/beleg', function (req, res) {
 	//verify that the payload is a push from the correct repo
 	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
 	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
-
-	console.log('pulling code from GitHub...');
-
-	// reset any changes that have been made locally
-	//exec('git -C ~/projects/wackcoon-device reset --hard', execCallback);
-
-	// and ditch any files that have been added locally too
-	//exec('git -C ~/projects/wackcoon-device clean -df', execCallback);
-
-	// now pull down the latest
-	//exec('git -C ~/projects/wackcoon-device pull -f', execCallback);
-
-	// and npm install with --production
-	//exec('npm -C ~/projects/wackcoon-device install --production', execCallback);
-
-	// and run tsc
-	//exec('tsc', execCallback);
-
+  if(req.body.pusher.name === 'Splashcom' && req.body.repository.name === 'studienarbeit-beleg') {
+    console.log('pulling code from GitHub...');
+    exec('git -C ~/git/studienarbeit-beleg pull -f', execCallback);
+  } else {
+    console.log('No Splashcom or wrong repo, no pull!')
+  }
   res.sendStatus(200);
   res.end();
 });
