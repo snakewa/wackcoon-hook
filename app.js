75e35c02ca6a1b3f46dbb5e36030bfea7c45e4f6
Christoph Heidelbach <christoph.heidelbach@yahoo.com>
2017-11-13 22:45:32 +0100

    Add studienarbeit-project to route

--- app.js
+++ app.js
@@ -29,11 +29,24 @@ app.post('/stua/beleg', function (req, res) {
   res.end();
 });
 
+app.post('/stua/project', function (req, res) {
+	console.log('#----' + req.body.head_commit.timestamp + '----#');
+	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
+  if(req.body.pusher.name === 'Splashcom' && req.body.repository.name === 'studienarbeit-project') {
+    console.log('pulling code from GitHub...');
+    exec('git -C ~/media/usb1/git/studienarbeit-project pull', execCallback);
+  } else {
+    console.log('No Splashcom or wrong repo, no pull!')
+  }
+  res.sendStatus(200);
+  res.end();
+});
+
 app.listen(5029, function () {
 	console.log('App listening on port 5029')
 });
 
 function execCallback(err, stdout, stderr) {
-	if(stdout) console.log(stdout);
-	if(stderr) console.log(stderr);
+	if (stdout) console.log(stdout);
+	if (stderr) console.log(stderr);
 }
